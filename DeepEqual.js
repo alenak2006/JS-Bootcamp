e3d651f4a76db1f1d1ad0c6c75ca811f1bf18d4c
Alena Kaleda <alenak06@hotmail.com>
2019-07-10 22:13:59 +0100

    Update to remove extra variable

--- DeepEqual.js
+++ DeepEqual.js
@@ -1,23 +1,44 @@
-function deepEqual(obj1, obj2) {
-    //check if number of properties are the same
-    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-        return false;
-    }
+'use strict'
 
-    let isEqual = true;
-    for (key in obj1) {
-        if (isEqual) {
-            if (obj1.hasOwnProperty([key]) && obj2.hasOwnProperty([key])) {
-                //if property is an object then recurse
-                if (typeof obj1[key] === "object") {
-                    isEqual = deepEqual(obj1[key], obj2[key]);
-                    continue;
-                }
-                //toString() will ensure that content of the Functions will also be compared instead of their reference value
-                if (obj1[key].toString() === obj2[key].toString()) {
-                    continue;
-                } return false;
-            } return false;
-        } return false;
-    } return isEqual;
+function deepEqual(obj1, obj2) {
+  // check if number of properties are the same
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false
+  }
+
+  for (let key in obj1) {
+    if (obj1.hasOwnProperty([key]) && obj2.hasOwnProperty([key])) {
+      // if property is an object then recurse
+      if (typeof obj1[key] === 'object') {
+        if (deepEqual(obj1[key], obj2[key])) {
+          continue
+        }
+        return false
+      }
+      // toString() will ensure that content of the Functions will also be compared instead of their reference value
+      if (obj1[key].toString() === obj2[key].toString()) {
+        continue
+      }
+      return false
+    }
+    return false
+  }
+  return true
 }
+
+let obj1 = {
+  name: "Misha",
+  order: {
+    price: 20
+  }
+};
+
+let obj2 = {
+  name: "Misha",
+  order: {
+    price: 20
+  }
+};
+
+deepEqual(obj1, obj2);
+console.log("hi");
