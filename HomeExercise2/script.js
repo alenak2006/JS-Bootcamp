d6d9db2ff2f807fbde7db1ec185d1d42caf6bbdd
Alena Kaleda <alenak06@hotmail.com>
2019-07-15 19:50:55 +0100

    Home Exercise created

--- HomeExercise2/script.js
+++ HomeExercise2/script.js
@@ -1,26 +1,31 @@
 'use strict';
 
 function highlight(table) {
-  console.log(table);
-
   let tbody = table.getElementsByTagName('tbody')[0];
   let trs = tbody.getElementsByTagName('tr');
 
-  let a = 0;
-  for (let item in trs) {
-    a++;
-    console.log(a);
-    console.log(trs[item].cells[3]);
-  }
   for (let i = 0; i < trs.length; i++) {
-    let statusCell = trs[i].cells[3];
-    console.log(statusCell);
+    let row = trs[i];
+    let ageCell = row.cells[1];
+    let genderCell = row.cells[2];
+    let statusCell = row.cells[3];
+
+    //add available/unavailable class to the row based on a value in Status column and data attribute and apply attribute hidden if unavailable
     if (statusCell.dataset.available) {
-      statusCell.addClass('available');
+      row.classList.add('available');
+    } else {
+      row.classList.add('unavailable');
+      row.hidden = true;
+    }
+
+    //add male/female class to the row based on a value in Gender column
+    (genderCell.innerText === 'm') ? row.classList.add('male') : row.classList.add('female');
+
+    //add style based on on a value in Age column
+    if (+ageCell.innerText < 18) {
+      row.style.textDecoration = "line-through"
     }
   }
-
-  console.log(trs);
 }
 
 highlight(document.querySelector('.js-teachers'));
