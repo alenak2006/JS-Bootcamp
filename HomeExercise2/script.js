a97a049e24c792f7521a2490db4e1d43ccf7e53d
Alena Kaleda <alenak06@hotmail.com>
2019-07-18 19:27:03 +0100

    Updated as suggested

--- HomeExercise2/script.js
+++ HomeExercise2/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
 function highlight(table) {
-  let tbody = table.getElementsByTagName('tbody')[0];
-  let trs = tbody.getElementsByTagName('tr');
+  let tbody = table.querySelectorAll('tbody')[0];
+  let trs = tbody.querySelectorAll('tr');
 
   for (let i = 0; i < trs.length; i++) {
     let row = trs[i];
-    let ageCell = row.cells[1];
-    let genderCell = row.cells[2];
+    let ageCell = row.cells[1].innerText;
+    let genderCell = row.cells[2].innerText.toLowerCase();
     let statusCell = row.cells[3];
 
     //add available/unavailable class to the row based on a value in Status column and data attribute and apply attribute hidden if unavailable
@@ -19,11 +19,19 @@ function highlight(table) {
     }
 
     //add male/female class to the row based on a value in Gender column
-    (genderCell.innerText === 'm') ? row.classList.add('male') : row.classList.add('female');
+    //(genderCell.innerText === 'm') ? row.classList.add('male') : row.classList.add('female');
+
+    if (genderCell === 'm') {
+      row.classList.add('male')
+    } else if (genderCell === 'f') {
+      row.classList.add('female');
+    }
 
     //add style based on on a value in Age column
-    if (+ageCell.innerText < 18) {
-      row.style.textDecoration = "line-through"
+    if (!isNaN(ageCell)) {
+      if (ageCell < 18) {
+        row.style.textDecoration = "line-through"
+      }
     }
   }
 }
