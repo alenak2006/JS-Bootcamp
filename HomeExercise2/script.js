8a9529806556386a5a3fb79de56b0f8a749cefda
Alena Kaleda <alenak06@hotmail.com>
2019-07-18 19:47:52 +0100

    Updated as suggested

--- HomeExercise2/script.js
+++ HomeExercise2/script.js
@@ -1,30 +1,33 @@
 'use strict';
 
 function highlight(table) {
-  let tbody = table.querySelectorAll('tbody')[0];
+  let tbody = table.querySelector('tbody');
   let trs = tbody.querySelectorAll('tr');
 
   for (let i = 0; i < trs.length; i++) {
     let row = trs[i];
     let ageCell = row.cells[1].innerText;
-    let genderCell = row.cells[2].innerText.toLowerCase();
-    let statusCell = row.cells[3];
+    let genderCell = row.cells[2].innerText;
+    let statusCell = row.cells[3].dataset.available;
 
     //add available/unavailable class to the row based on a value in Status column and data attribute and apply attribute hidden if unavailable
-    if (statusCell.dataset.available) {
-      row.classList.add('available');
+    if (statusCell) {
+      if (statusCell.toLowerCase() === "true") {
+        row.classList.add('available');
+      } else if (statusCell.toLowerCase() === "false") {
+        row.classList.add('unavailable');
+      }
     } else {
-      row.classList.add('unavailable');
       row.hidden = true;
     }
 
     //add male/female class to the row based on a value in Gender column
-    //(genderCell.innerText === 'm') ? row.classList.add('male') : row.classList.add('female');
-
-    if (genderCell === 'm') {
-      row.classList.add('male')
-    } else if (genderCell === 'f') {
-      row.classList.add('female');
+    if (genderCell) {
+      if (genderCell.toLowerCase() === 'm') {
+        row.classList.add('male')
+      } else if (genderCell.toLowerCase() === 'f') {
+        row.classList.add('female');
+      }
     }
 
     //add style based on on a value in Age column
